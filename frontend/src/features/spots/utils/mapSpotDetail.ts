49b2856d1ec92e4d91f5413791c4af8ee256692b
sss1-yyy <ksui9210@gmail.com>
2026-09-03 03:22:31 +0900

    fix : description에 더보기 추가

--- frontend/src/features/spots/utils/mapSpotDetail.ts
+++ frontend/src/features/spots/utils/mapSpotDetail.ts
@@ -6,17 +6,21 @@ export interface SpotDetailData {
   imageUrl: string;
   address: string;
   description: string;
+  sigunguCd: number;
+  sigunguNm: string;
   lat: number;
   lng: number;
 }
 
 export function mapSpotDetailToDetailData(item: SpotDetailItem): SpotDetailData {
   return {
-    id: String(item.spotId),
+    id: String(item.contentId),
     title: item.title,
     imageUrl: item.firstImage,
     address: item.addr1,
-    description: item.description,
+    description: item.overview,
+    sigunguCd:item.sigunguCd,
+    sigunguNm: item.sigunguNm,
     lat: item.mapY,   // mapY가 위도
     lng: item.mapX,   // mapX가 경도
   };
