f1440f49d0a3d598ef19dc7d6314ebe70b239518
suebin78 <ksb060708@hanyang.ac.kr>
2026-09-12 12:34:57 +0900

    chore : 폰트 변경

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,15 +19,22 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <html lang="ko" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <head>
+        <link
+            rel="stylesheet"
+            as="style"
+            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.css"
+        />
+      </head>
       <body className="min-h-full flex flex-col">
-        <Providers>{children}</Providers>
+      <Providers>{children}</Providers>
       </body>
-    </html>
+      </html>
   );
 }
